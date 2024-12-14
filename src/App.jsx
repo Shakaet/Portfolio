b0025b6fbc35aa186764c9f384757da0fd3fbd45
Shakaet <abdshakaet@gmail.com>
2024-12-15 01:25:31 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import AboutMe from "./components/AboutMe"
 import Banner from "./components/Banner"
+import Contact from "./components/Contact"
 import NavBar from "./components/NavBar"
 import Projects from "./components/Projects"
 import Skill from "./components/Skill"
@@ -17,6 +18,7 @@ function App() {
       <AboutMe></AboutMe>
       <Skill></Skill>
       <Projects></Projects>
+      <Contact></Contact>
      
      
     </>
